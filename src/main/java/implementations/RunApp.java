9948b470860b6d684db1f66aa2611d67b1222a36
juhasmu <juhasmustonen@gmail.com>
2018-10-11 14:57:09 +0300

    Stilisointia

--- src/main/java/implementations/RunApp.java
+++ src/main/java/implementations/RunApp.java
@@ -3,7 +3,6 @@ package implementations;
 import config.AppConfig;
 import interfaces.Country;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import implementations.Tallennus;
 
 import java.util.Scanner;
 
@@ -17,28 +16,38 @@ public class RunApp {
             System.out.println("Kirjoita 0 niin lopetetaan");
             System.out.print("Kirjoita jotain> ");
             luettu = lukija.nextLine();
-            System.out.println(luettu);
             if(luettu.contains("Finland")){
                 country = ctx.getBean(Finland.class);
                 if(luettu.equals("getFinland")){
-                    System.out.println(country.getName()+" population is "+country.getPopulation()+ " and president is "+country.getPresident());
+                    System.out.println("\nOfficial name of the coutry: "+country.getName());
+                    System.out.println ("Population count: "+country.getPopulation());
+                    System.out.println ("President: "+country.getPresident()+"\n");
+                    continue;
                 }
                 if(luettu.equals("setFinland")){
                    new Tallennus(country);
+                   System.out.println("");
+                   continue;
                 }
             }
             if(luettu.contains("UnitedStates")){
                 country = ctx.getBean(UnitedStates.class);
                 if(luettu.equals("getUnitedStates")){
-                    System.out.println(country.getName()+" population is "+country.getPopulation()+ " and president is  "+country.getPresident());
+                    System.out.println("\nOfficial name of the coutry: "+country.getName());
+                    System.out.println ("Population count: "+country.getPopulation());
+                    System.out.println ("President: "+country.getPresident()+"\n");
+                    continue;
                 }
                 if (luettu.equals("setUnitedStates")){
                     new Tallennus(country);
+                    System.out.println("");
+                    continue;
                 }
             }
             if(luettu.equals("0")) {
                 return;
             }
+            System.out.println("Anna databacesta löytyvä maa");
         }
     }
 }
