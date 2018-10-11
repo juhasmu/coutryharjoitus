e02e424c0aa929dcb8ab3f9e0336447a8f14e049
juhasmu <juhasmustonen@gmail.com>
2018-10-11 13:26:45 +0300

    County info when you write country name

--- src/main/java/implementations/RunApp.java
+++ src/main/java/implementations/RunApp.java
@@ -13,13 +13,17 @@ public class RunApp {
         AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(AppConfig.class);
         Country c1 =ctx.getBean(Finland.class);
         Country c2 =ctx.getBean(UnitedStates.class);
-        System.out.println(c1.getName()+" population is "+c1.getPopulation()+ " and president is "+c1.getPresident());
-        System.out.println(c2.getName()+" population is "+c2.getPopulation()+ " and president is  "+c2.getPresident());
         while(true) {
             System.out.println("Kirjoita 0 niin lopetetaan");
             System.out.print("Kirjoita jotain> ");
             luettu = lukija.nextLine();
             System.out.println(luettu);
+            if(luettu.equals(c1.getName())){
+                System.out.println(c1.getName()+" population is "+c1.getPopulation()+ " and president is "+c1.getPresident());
+            }
+            if(luettu.equals(c2.getName())){
+                System.out.println(c2.getName()+" population is "+c2.getPopulation()+ " and president is  "+c2.getPresident());
+            }
             if(luettu.equals("0")) {
                 return;
             }
