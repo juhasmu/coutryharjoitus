68b0f87437ad99d292022ca68428364c64b90414
juhasmu <juhasmustonen@gmail.com>
2018-10-11 13:38:50 +0300

    One country variable

--- src/main/java/implementations/RunApp.java
+++ src/main/java/implementations/RunApp.java
@@ -11,18 +11,19 @@ public class RunApp {
         String luettu;
         Scanner lukija = new Scanner(System.in);
         AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(AppConfig.class);
-        Country c1 =ctx.getBean(Finland.class);
-        Country c2 =ctx.getBean(UnitedStates.class);
+        Country country = null;
         while(true) {
             System.out.println("Kirjoita 0 niin lopetetaan");
             System.out.print("Kirjoita jotain> ");
             luettu = lukija.nextLine();
             System.out.println(luettu);
-            if(luettu.equals(c1.getName())){
-                System.out.println(c1.getName()+" population is "+c1.getPopulation()+ " and president is "+c1.getPresident());
+            if(luettu.equals("getFinland")){
+                country = ctx.getBean(Finland.class);
+                System.out.println(country.getName()+" population is "+country.getPopulation()+ " and president is "+country.getPresident());
             }
-            if(luettu.equals(c2.getName())){
-                System.out.println(c2.getName()+" population is "+c2.getPopulation()+ " and president is  "+c2.getPresident());
+            if(luettu.equals("getUnitedStates")){
+                country = ctx.getBean(UnitedStates.class);
+                System.out.println(country.getName()+" population is "+country.getPopulation()+ " and president is  "+country.getPresident());
             }
             if(luettu.equals("0")) {
                 return;
